337c4d935c3875b8ebafa69ef0780309ab3c876f
William0Dong <chuanhao.tung@gmail.com>
2022-03-26 16:15:59 -0400

    Finished first 15 opcode instructions

--- chip-8.c
+++ chip-8.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>  
 
 const unsigned int ADDR_INST_START = 0x200;
 const unsigned int NUM_CHAR = 80;
@@ -11,8 +12,11 @@ struct components {
     u_int8_t soundTimer;
     u_int16_t pc;
     u_int16_t stack[16];
-    u_int8_t registers;
+    u_int8_t registers[16];
     u_int8_t memory[4096];
+    u_int32_t video[64 * 32];
+    u_int8_t sp;
+    u_int16_t opcode;
 };
 struct components chip8;
 
@@ -37,13 +41,13 @@ u_int8_t characters[NUM_CHAR] = {
 
 
 void loadROM(char const* filename) {
-    FILE* binary_ROM = fopen(filename, "rb");
-    fseek(binary_ROM, 0, SEEK_END);
-    int size = ftell(binary_ROM);
+    FILE* binaryROM = fopen(filename, "rb");
+    fseek(binaryROM, 0, SEEK_END);
+    int size = ftell(binaryROM);
     char* buffer = malloc(size + 1);
-    fread(buffer, size, 1, binary_ROM);
-    rewind(binary_ROM);
-    fclose(binary_ROM);
+    fread(buffer, size, 1, binaryROM);
+    rewind(binaryROM);
+    fclose(binaryROM);
     for (long i = 0; i < size; ++i) {
         chip8.memory[ADDR_INST_START + i] = buffer[i];
     }
@@ -56,4 +60,161 @@ void Chip8Start() {
 	{
 		chip8.memory[ADDR_CHAR_START + i] = characters[i];
 	}
+}
+
+int randomByte() {
+    srand(time(0));
+    int randomNumber = rand() % (255 + 1);
+}
+
+// CLEAR DISPLAY (CLS)
+void OP_00E0() {
+    memset(chip8.video, 0, sizeof(chip8.video));
+}
+
+// RETURN FROM SUBROUTINE (RET)
+void OP_00EE(){
+    --chip8.sp;
+    chip8.pc = chip8.stack[chip8.sp];
+}
+
+// Jump to location (JP addr)
+void OP_1nnn()
+{
+	u_int16_t address = chip8.opcode & 0x0FFFu;
+
+	chip8.pc = address;
+}
+
+// Call subroutine (CALL addr)
+void OP_2nnn() {
+    u_int16_t address = chip8.opcode & 0x0FFFu;
+	chip8.stack[chip8.sp] = chip8.pc;
+	++chip8.sp;
+	chip8.pc = address;
+}
+
+// Skip next instruction if VX = KK (SE Vx, byte)
+void OP_3xkk() {
+	u_int8_t Vx = (chip8.opcode & 0x0F00u) >> 8u;
+	u_int8_t byte = chip8.opcode & 0x00FFu;
+
+	if (chip8.registers[Vx] == byte)
+	{
+		chip8.pc += 2;
+	}
+}
+
+// Skips next instruction if VX does not equal KK (SNE Vx, byte)
+void OP_4xkk()
+{
+	u_int8_t Vx = (chip8.opcode & 0x0F00u) >> 8u;
+	u_int8_t byte = chip8.opcode & 0x00FFu;
+
+	if (chip8.registers[Vx] != byte)
+	{
+		chip8.pc += 2;
+	}
+}
+
+// Skip next instruction if VX = VY (SE Vx, Vy)
+void OP_5xy0()
+{
+	u_int8_t Vx = (chip8.opcode & 0x0F00u) >> 8u;
+	u_int8_t Vy = (chip8.opcode & 0x00F0u) >> 4u;
+
+	if (chip8.registers[Vx] == chip8.registers[Vy])
+	{
+		chip8.pc += 2;
+	}
+}
+
+// Set Vx = kk (LD Vx, byte)
+void OP_6xkk()
+{
+	u_int8_t Vx = (chip8.opcode & 0x0F00u) >> 8u;
+	u_int8_t byte = chip8.opcode & 0x00FFu;
+	chip8.registers[Vx] = byte;
+}
+
+// Set Vx = Vx + kk. (ADD Vx, byte)
+void OP_7xkk()
+{
+	u_int8_t Vx = (chip8.opcode & 0x0F00u) >> 8u;
+	u_int8_t byte = chip8.opcode & 0x00FFu;
+
+	chip8.registers[Vx] += byte;
+}
+
+// Set Vx = Vy (LD Vx, Vy)
+void OP_8xy0()
+{
+	u_int8_t Vx = (chip8.opcode & 0x0F00u) >> 8u;
+	u_int8_t Vy = (chip8.opcode & 0x00F0u) >> 4u;
+
+	chip8.registers[Vx] = chip8.registers[Vy];
+}
+
+// Set Vx = Vx OR Vy. (OR Vx, Vy)
+void OP_8xy1()
+{
+	u_int8_t Vx = (chip8.opcode & 0x0F00u) >> 8u;
+	u_int8_t Vy = (chip8.opcode & 0x00F0u) >> 4u;
+
+	chip8.registers[Vx] |= chip8.registers[Vy];
+}
+
+// Set Vx = Vx AND Vy. (AND Vx, Vy)
+void OP_8xy2()
+{
+	u_int8_t Vx = (chip8.opcode & 0x0F00u) >> 8u;
+	u_int8_t Vy = (chip8.opcode & 0x00F0u) >> 4u;
+
+	chip8.registers[Vx] &= chip8.registers[Vy];
+}
+
+// Set Vx = Vx XOR Vy. (XOR Vx, Vy)
+void OP_8xy3()
+{
+	u_int8_t Vx = (chip8.opcode & 0x0F00u) >> 8u;
+	u_int8_t Vy = (chip8.opcode & 0x00F0u) >> 4u;
+
+	chip8.registers[Vx] ^= chip8.registers[Vy];
+}
+
+// Set Vx = Vx + Vy, set VF = carry (ADD Vx, Vy)
+void OP_8xy4()
+{
+	u_int8_t Vx = (chip8.opcode & 0x0F00u) >> 8u;
+	u_int8_t Vy = (chip8.opcode & 0x00F0u) >> 4u;
+
+	u_int16_t sum = chip8.registers[Vx] + chip8.registers[Vy];
+
+	if (sum > 255U)
+	{
+		chip8.registers[0xF] = 1;
+	}
+	else
+	{
+		chip8.registers[0xF] = 0;
+	}
+
+	chip8.registers[Vx] = sum & 0xFFu;
+}
+
+// Set Vx = Vx - Vy, set VF = NOT borrow. (SUB Vx, Vy)
+void OP_8xy5()
+{
+	u_int8_t Vx = (chip8.opcode & 0x0F00u) >> 8u;
+	u_int8_t Vy = (chip8.opcode & 0x00F0u) >> 4u;
+
+	if (chip8.registers[Vx] > chip8.registers[Vy])
+	{
+		chip8.registers[0xF] = 1;
+	}
+	else
+	{
+		chip8.registers[0xF] = 0;
+	}
+	chip8.registers[Vx] -= chip8.registers[Vy];
 }
